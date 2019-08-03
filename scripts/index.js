01c41f8fb2c9eca2937cfb3c6f0e7a64d3ff8018
Sérgio Moura <sergio.moura@eits.com.br>
2019-08-03 12:19:21 -0300

    Desenvolvimento template principal e paginação

---
+++ scripts/index.js
@@ -0,0 +1,99 @@
+
+    /*
+       
+    */
+
+    reportId ='a5b23673-5f90-4f8f-820f-69b0c7bc5b70';
+    groupId = 'cda676c6-6471-4a3c-89d7-26cd0f5a1910';
+    embedUrl = 'https://app.powerbi.com/reportEmbed?reportId=' + reportId +'&groupId=' + groupId;
+    expiration = '2019-02-27T19:53:50Z';
+    accesToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6InU0T2ZORlBId0VCb3NIanRyYXVPYlY4NExuWSIsImtpZCI6InU0T2ZORlBId0VCb3NIanRyYXVPYlY4NExuWSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYzVlZjQ5MTUtZjJkMy00MTU0LWFiN2UtNmIwZTliOWVmYTYyLyIsImlhdCI6MTU2NDg0Mzc3OSwibmJmIjoxNTY0ODQzNzc5LCJleHAiOjE1NjQ4NDc2NzksImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiI0MkZnWU5qZVYzYnh4SlBmd2RJZGMydHlOeDJPTVV2L3hKSVljcW1Sc1RUMnlKbW44ak1BIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6IjM4MDllNWJiLTlkMDYtNDdjYi1hY2MyLTUyYjg0YTg0YTYxYSIsImFwcGlkYWNyIjoiMSIsImZhbWlseV9uYW1lIjoiU0lTVEVNQVMiLCJnaXZlbl9uYW1lIjoiU1dBIiwiaXBhZGRyIjoiNDUuNC4xNS44NCIsIm5hbWUiOiJTV0EgU0lTVEVNQVMiLCJvaWQiOiIzYTA1MzNhZC0xZTQ5LTRjMmQtOTVmZC1hZGI4Zjc5ODAyMTkiLCJwdWlkIjoiMTAwMzIwMDAzN0Q3QkU5NyIsInNjcCI6IkFwcC5SZWFkLkFsbCBDYXBhY2l0eS5SZWFkLkFsbCBDYXBhY2l0eS5SZWFkV3JpdGUuQWxsIENvbnRlbnQuQ3JlYXRlIERhc2hib2FyZC5SZWFkLkFsbCBEYXNoYm9hcmQuUmVhZFdyaXRlLkFsbCBEYXRhLkFsdGVyX0FueSBEYXRhZmxvdy5SZWFkLkFsbCBEYXRhZmxvdy5SZWFkV3JpdGUuQWxsIERhdGFzZXQuUmVhZC5BbGwgRGF0YXNldC5SZWFkV3JpdGUuQWxsIEdhdGV3YXkuUmVhZC5BbGwgR2F0ZXdheS5SZWFkV3JpdGUuQWxsIEdyb3VwLlJlYWQgR3JvdXAuUmVhZC5BbGwgTWV0YWRhdGEuVmlld19BbnkgUmVwb3J0LlJlYWQuQWxsIFJlcG9ydC5SZWFkV3JpdGUuQWxsIFN0b3JhZ2VBY2NvdW50LlJlYWQuQWxsIFN0b3JhZ2VBY2NvdW50LlJlYWRXcml0ZS5BbGwgVGVuYW50LlJlYWQuQWxsIFRlbmFudC5SZWFkV3JpdGUuQWxsIFdvcmtzcGFjZS5SZWFkLkFsbCBXb3Jrc3BhY2UuUmVhZFdyaXRlLkFsbCIsInN1YiI6ImRSaUdLYjBLSzJQaWpBQ3NNd3RLQ1J2cEQ2RnR6WFhHV2hCT1Z1YzREZWMiLCJ0aWQiOiJjNWVmNDkxNS1mMmQzLTQxNTQtYWI3ZS02YjBlOWI5ZWZhNjIiLCJ1bmlxdWVfbmFtZSI6InBvd2VyYmlAc3dhLmNvbS5iciIsInVwbiI6InBvd2VyYmlAc3dhLmNvbS5iciIsInV0aSI6IkI2cmxqMXNmaWt1cG93bGdVQVVXQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbIjYyZTkwMzk0LTY5ZjUtNDIzNy05MTkwLTAxMjE3NzE0NWUxMCJdfQ.OcOuKVDiIfaFjAYVSohGwys0UkwSYW59Y1UvoMcbtPh1VJD1UToaESL5vdZ_K43zu7tN9EkDnkBL-zUH01haE_TxH65-iipwGaTLGRonaQkb6ui56wIvOgkige2ZHLggJKk05aUkGyGT42zzWp6ZhHIkTLlBKnqXCiWaxtyb-WqF2BDpY6W25A-uf2BIP0wRtD6whpAuSl-D3n0Oz1JMQ7tOICBNkQBGyBPkvt28E5hBw4WzYcEwqBaPvr-XlUT155O0RbqK9lozethbgVbxx5MxEBMIbiNMnXBzTUwALgCvblT_t_8PQP9Q4iE6CowRLIS0tRsu5huJ9VkBnLayig';
+    arrayPages = {};
+    // ---- Embed Code ----------------------------------------------------
+
+    function _Print() {
+        report.fullscreen();
+    }
+
+   async function _GetPages(){
+        lala = "";
+        setorAnterior = "";
+        try{
+            response = await fetch('https://api.powerbi.com/v1.0/myorg/groups/'  + groupId + '/reports/' + reportId + '/pages', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + accesToken
+                }
+                }).then(function(response){
+                    response.json().then(function(result) {
+                        lala = result;
+                        $.each( result.value.sort(function(a, b){return a.order - b.order}), function(){
+                            console.log(this.Name + " " + this.displayName );
+                            if (this.order > 0){
+                                const setor =  this.displayName.split('-');
+                                if (setorAnterior == "" || setor[0] != setorAnterior){
+                                    document.getElementById("reportPages").innerHTML +=
+                                    '<div class="dropdown-divider"></div>' + 
+                                               '<h6 class="dropdown-header">' + setor[0] + '</h6>' +
+                                               '<div class="dropdown-divider"></div>'
+                                }
+                                setorAnterior = setor[0];
+                                document.getElementById("reportPages").innerHTML +=
+                                '<a class="dropdown-item" id="' + this.Name + '" onclick="_ChangePage();">' + setor[1] + '</a>'
+                            }
+                        });
+                    });
+                });
+        } catch(err){
+            console.log(err);
+        }
+        
+    }
+
+    function _ChangePage() {
+        const page = report.page(event.target.id);
+        page.setActive();
+    }
+
+    function _EmbedReport() {
+
+            // Get models. models contains enums that can be used.
+            var models = window['powerbi-client'].models;
+
+            // We give All permissions to demonstrate switching between View and Edit mode and saving report.
+            var permissions = models.Permissions.All;
+                
+            // set config for embedding report
+            var config= {
+                type: 'report',
+                tokenType: models.TokenType.Aad,
+                accessToken: accesToken,
+                embedUrl: embedUrl,
+                id: reportId,
+                permissions: permissions,
+                pageName: 'ReportSection9268d7d4e0e8f87df5ca',
+                settings: {
+                    navContentPaneEnabled: false,
+                    filterPaneEnabled: false
+                }
+            };
+
+            // Get a reference to the embedded report HTML element
+            powerbi.enableAutoEmbed();
+            var reportContainer = document.getElementById('reportContainer');
+
+            // Embed the report and display it within the div container.
+            report = powerbi.embed(reportContainer, config);
+            
+            // Report.off removes a given event handler if it exists.        
+            report.off("loaded");
+
+            // Report.on will add an event handler which prints to Log window.
+            report.on("loaded", function() {
+            });
+            _GetPages();
+    }
+
+
+
